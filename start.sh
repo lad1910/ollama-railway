5632cbe00603418752aae341b239c1eecc38994d
lad1910 <aleksei@easymm.io>
2025-06-23 09:13:01 +0700

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting Ollama on port ${PORT:-11434}..."
-
-# Set the host environment variable instead of using --host flag
-export OLLAMA_HOST="0.0.0.0:${PORT:-11434}"
-
-# Start Ollama
-exec ollama serve
